039e37746bdc595b0f81fc0bba51f4b00dff6176
milleriishaun <milleriishaun@gmail.com>
2019-02-05 13:19:22 -0800

    Add benchmarking.js

---
+++ benchmarking.js
@@ -0,0 +1,94 @@
+// Benchmarking asychronous code
+var Benchmark = require('benchmark');
+
+var suite = new Benchmark.Suite();
+/*
+// the following compares finding o with regex versus using indexOf
+// add tests
+suite
+  .add('RegExp#test', function() {
+    /o/.test('Hello World!');
+  })
+  .add('String#indexOf', function() {
+    'Hello World!'.indexOf('o') > -1;
+  })
+  // add listeners
+  .on('cycle', function(event) {
+    console.log(String(event.target));
+  })
+  .on('complete', function() {
+    console.log('Fastest is ' + this.filter('fastest').map('name'));
+  })
+  // run async
+  .run({ async: true });
+*/
+
+// the cycle information really helps get more information about the
+// way to solve for what is faster... and it is nice to see results chosen.
+
+// Benchmarking in different browsers(use JSperf website)
+//example:
+const mySolution = n => {
+  let sum = 0;
+
+  for (let i = 0; i < n; i++) if (i % 3 === 0 || i % 5 === 0) sum += i;
+  return sum;
+};
+
+//vs
+const otherSolution = n => {
+  let number = n - 1;
+  const fives = Math.floor(number / 5), // # of multiples of 5 below n
+    threes = Math.floor(number / 3),
+    fifteens = Math.floor(number / 15),
+    sum = (base, num) => (base * (num ** 2 + num)) / 2;
+  return sum(5, fives) + sum(3, threes) - sum(15, fifteens);
+};
+
+// Using benchmark.js
+suite
+  .add('t1', function() {
+    mySolution(100000);
+  })
+  .add('t2', function() {
+    otherSolution(100000);
+  })
+  // add listeners
+  .on('cycle', function(event) {
+    console.log(String(event.target));
+  })
+  .on('complete', function() {
+    console.log('Fastest is ' + this.filter('fastest').map('name'));
+  })
+  // run async
+  .run({ async: true });
+
+// Benchmarking in Chrome Browser(improvised method)
+// comparing mySolution to otherSolution(answer in milliseconds)
+let then = new Date();
+for (let i = 0; i < 1000; i++) {
+  mySolution(100000);
+}
+let now = new Date();
+now - then;
+
+then = new Date(); //then and now were already declared
+for (let i = 0; i < 1000; i++) {
+  otherSolution(100000);
+}
+now = new Date();
+now - then;
+
+// Benchmark in Chrome Browser.(easy method)
+const bench = (func, input, iterations) => {
+  /* any boilerplate code you want to have happen before the timer starts, perhaps copying a variable so it isn't mutated */
+  const start = new Date();
+  for (let i = 0; i < iterations; i++) {
+    func(input);
+  }
+  const finish = new Date();
+  return finish - start;
+};
+//bench(functionName, input, numIters) in chrome console.log(ctrl+shft+J)
+//bench(mySolution, 100000, 1000);
+//bench(otherSolution, 100000, 1000);
--- c1_isUnique.js
+++ c1_isUnique.js
@@ -1,7 +1,29 @@
+var Benchmark = require('benchmark');
+
+var suite = new Benchmark.Suite();
+
+// add tests
+suite
+  .add('RegExp#test', function() {
+    /o/.test('Hello World!');
+  })
+  .add('String#indexOf', function() {
+    'Hello World!'.indexOf('o') > -1;
+  })
+  // add listeners
+  .on('cycle', function(event) {
+    console.log(String(event.target));
+  })
+  .on('complete', function() {
+    console.log('Fastest is ' + this.filter('fastest').map('name'));
+  })
+  // run async
+  .run({ async: true });
+
 // try 1
 // time: O(n^2)
 // space: O(1)
-function isUnique(str) {
+function issUnique(str) {
   let arr = str.split('');
   for (let i = 0; i < arr.length - 1; i++) {
     for (let j = i + 1; j < arr.length; j++) {
@@ -12,19 +34,29 @@ function isUnique(str) {
   }
   return true;
 }
-//benchmark: 3.022ms(too slow)
-console.time('with very few records in the map');
-console.log('striggnp', isUnique('striggnp'));
-console.timeEnd('with very few records in the map');
 
+//benchmark: 3.144ms(too slow)
+console.time('t1');
+for (let i = 0; i < 10000; i++) {
+  isUnique('striggnp');
+}
+console.timeEnd('t1');
+console.time('t2');
+for (let i = 0; i < 10000; i++) {
+  isUnique('striggnp');
+}
+console.timeEnd('t2');
+console.time('t2.1');
+for (let i = 0; i < 10000; i++) {
+  isUnique('striggnp');
+}
+console.timeEnd('t2.1');
 // try 2
 // time: O(n)
 // space: O(1)
 function isUnique(str) {
-  let arr = [128];
-  for (let i = 0; i < arr.length; i++) {
-    arr[i] = true;
-  }
+  let arr = []; //I don't need to identify 128 in the []
+  // because JS arrays expand to fit, making any # of possible chars handled.
   for (let j = 0; j < str.length; j++) {
     let val = str.charAt(j);
     if (arr[val]) {
@@ -37,9 +69,45 @@ function isUnique(str) {
 }
 
 //tests
-//benchmark 1.155ms(much faster)
-console.time('with very few records in the map');
-console.log('striggnp', isUnique('striggnp'));
-console.timeEnd('with very few records in the map');
+//benchmark 0.574ms(much faster)(w/o console.log)
+//note: w/o console.log, the difference in speed is harder to
+//see, and can often be too close to tell.
+//benchmark 1.155ms(much faster)(w/ console.log)
+//note: now was see the difference between O(n^2) and O(n)
+console.time('t3');
+for (let i = 0; i < 10000; i++) {
+  isUnique('striggnp'); //your code here
+}
+console.timeEnd('t3');
+console.time('t4');
+for (let i = 0; i < 10000; i++) {
+  isUnique('striggnp');
+}
+console.timeEnd('t4');
 
-// try 3
+// try 3, with bitwise operators
+//note: idk how to use bitwise well, so mostly copying
+function isUnique(str) {
+  let checker = 0;
+  for (let i = 0; i < str.length; i++) {
+    let val = str.charAt(i) - 'a';
+    if ((checker & (1 << val)) > 0) {
+      return false;
+    }
+    checker |= 1 << val;
+  }
+  return true;
+}
+
+//benchmark: 0.157ms(fastest)(w/o console.log)
+//benchmark: 0.426ms(fastest)(w/ console.log)
+console.time('t5');
+for (let i = 0; i < 10000; i++) {
+  isUnique('striggnp');
+}
+console.timeEnd('t5');
+console.time('t6');
+for (let i = 0; i < 10000; i++) {
+  isUnique('striggnp');
+}
+console.timeEnd('t6');
