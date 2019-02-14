058a87d0928bd80d0fa8f6353e7105774cba257b
milleriishaun <milleriishaun@gmail.com>
2019-02-13 20:05:52 -0800

    Add benchmarked solutions

--- c1_isUnique.js
+++ c1_isUnique.js
@@ -1,25 +1,100 @@
 var Benchmark = require('benchmark');
 var suite = new Benchmark.Suite();
 
-// try 1
-// time: O(n^2)
-// space: O(1)
+// O(N log N) TIME --- O(1) SPACE
 function isUnique1(str) {
-  let arr = str.split('');
-  for (let i = 0; i < arr.length - 1; i++) {
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[i] === arr[j]) {
-        return false;
-      }
+  str = str.split``.sort();
+  const length = str.length;
+
+  for (let i = 0; i < length; i++) {
+    if (str[i] === str[i + 1]) return false;
+  }
+
+  return true;
+}
+
+// Time: O(N lg N)
+// Additional space: O(1)
+// Sort the original string first then iterate through it. Repeat characters
+// will show up next to eachother so fail if any two characters in a row
+// are the same.
+function isUnique2(str) {
+  // sort string using quicksort
+  arr = str.split('').sort();
+  for (var i = 1; i < arr.length; ++i) {
+    if (arr[i] === arr[i - 1]) {
+      return false;
     }
   }
+  return true;
+}
+
+// Using a Set
+const isUnique3 = str => new Set(str).size === str.length;
+
+// ctci js
+function isUnique4(str) {
+  // ASCII string longer than 128 has dups
+  if (str.length > 128) {
+    return false;
+  }
+  // Set makes this easy 😀
+  return new Set(str).size == str.length;
+}
+
+// JS Algos2
+function isUnique5(str) {
+  var hash = {};
+  for (var i = 0; i < str.length; i++) {
+    var ele = str[i];
+    if (!hash[ele]) {
+      hash[ele] = 1;
+    } else {
+      hash[ele]++;
+    }
+  }
+  for (var key in hash) {
+    if (hash[key] > 1) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// CTCI ES5
+// Time: O(N)
+// Additional space: O(N)
+// Keep track of seen characters with a Set data structure, fail when
+// a repeated character is found.
+function isUnique6(str) {
+  let chars = new Set();
+
+  for (let i = 0; i < str.length; ++i) {
+    if (chars.has(str[i])) {
+      return false;
+    }
+    chars.add(str[i]);
+  }
+  return true;
+}
+
+// CTCI JS Sols3
+// O(N) TIME --- O(N) SPACE
+function isUnique7(str) {
+  const letterSet = new Set();
+
+  for (const letter of str) {
+    if (letterSet.has(letter)) return false;
+    letterSet.add(letter);
+  }
+
   return true;
 }
 
 // try 2
 // time: O(n)
 // space: O(n) // since storing
-function isUnique2(str) {
+function isUnique8(str) {
   let arr = []; //I don't need to identify 128 in the []
   // because JS arrays expand to fit, making any # of possible chars handled.
   for (let j = 0; j < str.length; j++) {
@@ -33,31 +108,36 @@ function isUnique2(str) {
   return true;
 }
 
-// try 3, with bitwise operators
-//note: idk how to use bitwise well, so mostly copying
-// time: O(n)
-// space: O(1) // since not storing
-function isUnique3(str) {
-  let checker = 0;
-  for (let i = 0; i < str.length; i++) {
-    let val = str.charCodeAt(i) - 'a'.charCodeAt(0);
-    // console.log('val', val);
-    // console.log('1 << val', 1 << val);
-    if ((checker & (1 << val)) > 0) {
+// JS Algos1
+function isUnique9(str) {
+  var element = {};
+  for (var i = 0; i < str.length; i++) {
+    if (element[str[i]]) {
       return false;
     }
-    // console.log(checker);
-    checker |= 1 << val;
-    // console.log(checker);
+    element[str[i]] = true;
   }
   return true;
 }
-// console.log(isUnique3('striggnp'));
+
+// algo sols
+function isUnique10(str) {
+  if (str.length > 128) {
+    return false;
+  }
+  let checked = {};
+  for (let i = 0; i < str.length; i++) {
+    let elm = str.charAt(i);
+    if (checked[elm]) return false;
+    checked[elm] = true;
+  }
+  return true;
+}
 
 // ChirpingmermaidCodes... ES6
 // time: O(n)
 // space: O(n)
-let isUnique4 = s => {
+let isUnique11 = s => {
   // check each character and store in a hash table
   // if we find that the character is already there, return false
   // when done checking, return true
@@ -74,12 +154,272 @@ let isUnique4 = s => {
   return true;
 };
 
-console.log(isUnique4('s') === true);
-console.log(isUnique4('') === true);
-console.log(isUnique4('ss') === false);
-console.log(isUnique4('stories') === false);
-console.log(isUnique4('rainbow') === true);
-console.log(isUnique4('chirpingmermaid') === false);
+// CTCI JS Sols2
+// O(Nˆ2) TIME --- O(1) SPACE
+function isUnique12(str) {
+  const strLength = str.length;
+
+  for (let i = 0; i < strLength; i++) {
+    for (let x = i + 1; x < strLength; x++) {
+      if (str[i] === str[x]) return false;
+    }
+  }
+
+  return true;
+}
+
+// ctci
+function isUnique13(str) {
+  if (!str.length) return true; // fail early if empty string
+
+  const memo = {};
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (memo[char]) return false;
+    memo[char] = 1;
+  }
+
+  return true;
+}
+
+// try 1
+// time: O(n^2)
+// space: O(1)
+function isUnique14(str) {
+  let arr = str.split('');
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] === arr[j]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+// same as above but without data-structures
+function isUnique15(str) {
+  // ASCII string longer than 128 has dups
+  if (str.length > 128) {
+    return false;
+  }
+
+  // O(n2) when banning use of hash or array etc
+  for (let i = 0; i < str.length; i += 1) {
+    for (let j = i + 1; j < str.length; j += 1) {
+      if (str[i] === str[j]) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
+
+// CTCI JS
+var isUnique16 = function(string) {
+  // O(n^2) approach, no additional data structures used
+  // for each character, check remaining characters for duplicates
+  for (var i = 0; i < string.length; i++) {
+    for (var j = i + 1; j < string.length; j++) {
+      if (string[i] === string[j]) {
+        return false; // if match, return false
+      }
+    }
+  }
+  return true; // if no match, return true
+};
+
+// CTCI JS Sols1
+// Regex
+const isUnique17 = str => !/(.).*\1/.test(str);
+
+// try 3, with bitwise operators
+//note: idk how to use bitwise well, so mostly copying
+// time: O(n)
+// space: O(1) // since not storing
+function isUnique18(str) {
+  let checker = 0;
+  for (let i = 0; i < str.length; i++) {
+    let val = str.charCodeAt(i) - 'a'.charCodeAt(0);
+    if ((checker & (1 << val)) > 0) {
+      return false;
+    }
+    checker |= 1 << val;
+  }
+  return true;
+}
+
+console.log(
+  isUnique1('s') === true,
+  isUnique1('') === true,
+  isUnique1('rainbow') === true,
+  isUnique1('ss') === false,
+  isUnique1('stories') === false,
+  isUnique1('chirpingmermaid') === false
+);
+
+console.log(
+  isUnique2('s') === true,
+  isUnique2('') === true,
+  isUnique2('rainbow') === true,
+  isUnique2('ss') === false,
+  isUnique2('stories') === false,
+  isUnique2('chirpingmermaid') === false
+);
+
+console.log(
+  isUnique3('s') === true,
+  isUnique3('') === true,
+  isUnique3('rainbow') === true,
+  isUnique3('ss') === false,
+  isUnique3('stories') === false,
+  isUnique3('chirpingmermaid') === false
+);
+
+console.log(
+  isUnique4('s') === true,
+  isUnique4('') === true,
+  isUnique4('rainbow') === true,
+  isUnique4('ss') === false,
+  isUnique4('stories') === false,
+  isUnique4('chirpingmermaid') === false
+);
+
+console.log(
+  isUnique5('s') === true,
+  isUnique5('') === true,
+  isUnique5('rainbow') === true,
+  isUnique5('ss') === false,
+  isUnique5('stories') === false,
+  isUnique5('chirpingmermaid') === false
+);
+
+console.log(
+  isUnique6('s') === true,
+  isUnique6('') === true,
+  isUnique6('rainbow') === true,
+  isUnique6('ss') === false,
+  isUnique6('stories') === false,
+  isUnique6('chirpingmermaid') === false
+);
+
+console.log(
+  isUnique7('s') === true,
+  isUnique7('') === true,
+  isUnique7('rainbow') === true,
+  isUnique7('ss') === false,
+  isUnique7('stories') === false,
+  isUnique7('chirpingmermaid') === false
+);
+
+console.log(
+  isUnique8('s') === true,
+  isUnique8('') === true,
+  isUnique8('rainbow') === true,
+  isUnique8('ss') === false,
+  isUnique8('stories') === false,
+  isUnique8('chirpingmermaid') === false
+);
+
+console.log(
+  isUnique9('s') === true,
+  isUnique9('') === true,
+  isUnique9('rainbow') === true,
+  isUnique9('ss') === false,
+  isUnique9('stories') === false,
+  isUnique9('chirpingmermaid') === false
+);
+
+console.log(
+  isUnique10('s') === true,
+  isUnique10('') === true,
+  isUnique10('rainbow') === true,
+  isUnique10('ss') === false,
+  isUnique10('stories') === false,
+  isUnique10('chirpingmermaid') === false
+);
+
+console.log(
+  isUnique11('s') === true,
+  isUnique11('') === true,
+  isUnique11('rainbow') === true,
+  isUnique11('ss') === false,
+  isUnique11('stories') === false,
+  isUnique11('chirpingmermaid') === false
+);
+
+console.log(
+  isUnique12('s') === true,
+  isUnique12('') === true,
+  isUnique12('rainbow') === true,
+  isUnique12('ss') === false,
+  isUnique12('stories') === false,
+  isUnique12('chirpingmermaid') === false
+);
+
+console.log(
+  isUnique13('s') === true,
+  isUnique13('') === true,
+  isUnique13('rainbow') === true,
+  isUnique13('ss') === false,
+  isUnique13('stories') === false,
+  isUnique13('chirpingmermaid') === false
+);
+
+console.log(
+  isUnique14('s') === true,
+  isUnique14('') === true,
+  isUnique14('rainbow') === true,
+  isUnique14('ss') === false,
+  isUnique14('stories') === false,
+  isUnique14('chirpingmermaid') === false
+);
+
+console.log(
+  isUnique15('s') === true,
+  isUnique15('') === true,
+  isUnique15('rainbow') === true,
+  isUnique15('ss') === false,
+  isUnique15('stories') === false,
+  isUnique15('chirpingmermaid') === false
+);
+
+console.log(
+  isUnique16('s') === true,
+  isUnique16('') === true,
+  isUnique16('rainbow') === true,
+  isUnique16('ss') === false,
+  isUnique16('stories') === false,
+  isUnique16('chirpingmermaid') === false
+);
+
+console.log(
+  isUnique17('s') === true,
+  isUnique17('') === true,
+  isUnique17('rainbow') === true,
+  isUnique17('ss') === false,
+  isUnique17('stories') === false,
+  isUnique17('chirpingmermaid') === false
+);
+
+console.log(
+  isUnique18('s') === true,
+  isUnique18('') === true,
+  isUnique18('rainbow') === true,
+  isUnique18('ss') === false,
+  isUnique18('stories') === false,
+  isUnique18('chirpingmermaid') === false
+);
+
+// console.log(
+//   isUnique('s') === true,
+//   isUnique('') === true,
+//   isUnique('rainbow') === true,
+//   isUnique('ss') === false,
+//   isUnique('stories') === false,
+//   isUnique('chirpingmermaid') === false
+// );
 
 // add tests
 suite
@@ -92,8 +432,50 @@ suite
   .add('isUnique3', function() {
     isUnique3('striggnp');
   })
-  .add('isUnique3', function() {
-    isUnique3('striggnp');
+  .add('isUnique4', function() {
+    isUnique4('striggnp');
+  })
+  .add('isUnique5', function() {
+    isUnique5('striggnp');
+  })
+  .add('isUnique6', function() {
+    isUnique6('striggnp');
+  })
+  .add('isUnique7', function() {
+    isUnique7('striggnp');
+  })
+  .add('isUnique8', function() {
+    isUnique8('striggnp');
+  })
+  .add('isUnique9', function() {
+    isUnique9('striggnp');
+  })
+  .add('isUnique10', function() {
+    isUnique10('striggnp');
+  })
+  .add('isUnique11', function() {
+    isUnique11('striggnp');
+  })
+  .add('isUnique12', function() {
+    isUnique12('striggnp');
+  })
+  .add('isUnique13', function() {
+    isUnique13('striggnp');
+  })
+  .add('isUnique14', function() {
+    isUnique14('striggnp');
+  })
+  .add('isUnique15', function() {
+    isUnique15('striggnp');
+  })
+  .add('isUnique16', function() {
+    isUnique16('striggnp');
+  })
+  .add('isUnique17', function() {
+    isUnique17('striggnp');
+  })
+  .add('isUnique18', function() {
+    isUnique18('striggnp');
   })
   // add listeners
   .on('cycle', function(event) {
---
+++ c1_oneAway.js
@@ -0,0 +1,365 @@
+var Benchmark = require('benchmark');
+var suite = new Benchmark.Suite();
+
+suite
+  .add('oneAway1', function() {
+    oneAway1('pale', 'bale');
+  })
+  .add('oneAway2', function() {
+    oneAway2('pale', 'bale');
+  })
+  .add('oneAway3', function() {
+    oneAway3('pale', 'bale');
+  })
+  .add('oneAway4', function() {
+    oneAway4('pale', 'bale');
+  })
+  .add('oneAway5', function() {
+    oneAway5('pale', 'bale');
+  })
+  .add('oneAway6', function() {
+    oneAway6('pale', 'bale');
+  })
+  .add('oneAway7', function() {
+    oneAway7('pale', 'bale');
+  })
+  .on('cycle', function(event) {
+    console.log(String(event.target));
+  })
+  .on('complete', function() {
+    console.log('Fastest is ' + this.filter('fastest').map('name'));
+  })
+  .run({ async: true });
+
+/**
+ * ONE AWAY
+ *
+ *
+ * I: string
+ * O: boolean
+ * C: optimize
+ * E: empty string
+ */
+
+// time complexity: O(n)
+// space complexity: O(n)
+
+// try 1
+function oneAway1(s1, s2) {
+  // could be 3 loops within each other, each checking one edit for all chars
+  // but this would be O(n^3) which would be inefficient.
+  // Another method: pick the longest string, find index of difference, then
+  // perform operations on it. Or, if 2 differences occur, then likely there is
+  // more than one edit necessary.
+  if (s2.length > s1.length) {
+    let temp = s1;
+    s1 = s2;
+    s2 = temp;
+  }
+  let arr1 = s1.split('');
+  let arr2 = s2.split('');
+  let count = 0;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      // if the 2 chars are not equal, if s2 correct char is either forward 1,
+      // or back 1. And if not, then there are more than 2 edits, so exit.
+      if (arr2[i + 1] === arr1[i]) {
+        arr2.splice(i, 1);
+        i--;
+      } else if (arr2[i] === arr1[i + 1]) {
+        arr2.splice(i, 0, arr1[i]);
+        i--;
+      } else if (arr2[i] !== arr1[i]) {
+        arr2[i] = arr1[i];
+      } else {
+        return false;
+      }
+      count++;
+    }
+    if (count > 1) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// ctci javascript
+function oneAway2(left, right) {
+  // lengths differ by more than one?
+  if (Math.abs(left.length - right.length) > 1) {
+    return false;
+  }
+
+  for (
+    let diff = 0, indexLeft = 0, indexRight = 0;
+    indexLeft < left.length || indexRight < right.length;
+    ++indexLeft, ++indexRight
+  ) {
+    if (left[indexLeft] !== right[indexRight]) {
+      // is there more than one diff?
+      if (++diff === 2) {
+        return false;
+      }
+      // when the current chars differ, increment R/L index if appropriate
+      if (left[indexLeft + 1] === right[indexRight]) {
+        // char inserted in left string or removed from right
+        ++indexLeft;
+      } else if (left[indexLeft] === right[indexRight + 1]) {
+        // char inserted in right string, or removed from left
+        ++indexRight;
+      }
+    }
+  }
+
+  return true;
+}
+
+// CTCI-Javascript
+var oneAway3 = function(string1, string2) {
+  // insert a char for str1 -> remove a char for str2
+  var checkOneMissing = function(first, second) {
+    if (first.length !== second.length - 1) {
+      return false;
+    } else {
+      var mulligan = false;
+      var fP = 0; // first Pointer
+      var sP = 0; // second Pointer
+      while (fP < first.length) {
+        if (first[fP] !== second[sP]) {
+          if (mulligan) {
+            return false;
+          } else {
+            mulligan = true;
+            sP++; // second length is longer
+          }
+        } else {
+          fP++;
+          sP++;
+        }
+      }
+      return true;
+    }
+  };
+
+  var checkOneDiff = function(first, second) {
+    if (first.length !== second.length) {
+      return false;
+    } else {
+      var mulligan = false;
+      var fP = 0; // first Pointer
+      var sP = 0; // second Pointer
+      while (fP < first.length) {
+        if (first[fP] !== second[sP]) {
+          if (mulligan) {
+            return false; // more than one mismatch
+          } else {
+            mulligan = true; // use up mulligan
+          }
+        }
+        fP++;
+        sP++;
+      }
+      return true;
+    }
+  };
+  // insert a char for str1 -> remove a char for str2
+  // check one diff
+
+  // console log checks
+  // console.log(string1, string2, 'checkMiss', checkOneMissing(string1, string2));
+  // console.log(string2, string1, 'checkMiss', checkOneMissing(string2, string1));
+  // console.log(string1, string2, 'checkDiff', checkOneDiff(string1, string2));
+
+  return (
+    checkOneMissing(string1, string2) ||
+    checkOneMissing(string2, string1) ||
+    checkOneDiff(string1, string2)
+  );
+};
+
+//CTCI JS Sols
+function oneAway4(str1, str2) {
+  const str1Length = str1.length,
+    str2Length = str2.length;
+
+  if (Math.abs(str1Length - str2Length) > 1) return false;
+
+  const isInsertion = str1Length < str2Length,
+    isDeletedChar = !isInsertion && str1Length !== str2Length;
+
+  let isEdited = false,
+    i,
+    x;
+
+  for (i = x = 0; i < str1Length && x < str2Length; i++, x++) {
+    if (str1[i] !== str2[x]) {
+      if (isEdited) return false;
+      if (isInsertion) i--;
+      else if (isDeletedChar) x--;
+      isEdited = true;
+    }
+  }
+
+  return true;
+}
+
+// ctci
+function oneAway5(a, b) {
+  if (!a.length || !b.length) return null;
+  if (Math.abs(a.length - b.length) > 1) return false;
+
+  if (a.length === b.length) return replace(a, b);
+  if (a.length < b.length) {
+    return insert(a, b);
+  } else {
+    return insert(b, a);
+  }
+}
+
+function replace(a, b) {
+  let edit = false;
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      if (edit) return false;
+      edit = true;
+    }
+  }
+
+  return true;
+}
+
+function insert(a, b) {
+  let i = 0;
+  let j = 0;
+  while (i < a.length && j < b.length) {
+    if (a[i] !== b[j]) {
+      if (i !== j) return false;
+      j++;
+    } else {
+      i++;
+      j++;
+    }
+  }
+
+  return true;
+}
+
+// CTCI ES5
+function oneAway6(str1, str2) {
+  // if lengths differ by more than 1 then can't be true
+  if (Math.abs(str1.length - str2.length) > 1) {
+    return false;
+  }
+
+  let isEdited = false;
+  for (let i = 0, j = 0; i < str1.length && j < str2.length; ++i, ++j) {
+    if (str1[i] !== str2[j]) {
+      if (isEdited) {
+        // second edit
+        return false;
+      }
+
+      if (str1.length > str2.length) {
+        --j; // decrease j, we are deleting char from str1
+      } else if (str1.length < str2.length) {
+        --i; // decrease i, we are deleting char from str2
+      }
+      isEdited = true;
+    }
+  }
+
+  return true;
+}
+
+// stackhouse
+function oneAway7(s1, s2) {
+  // Cracking the Coding Interview 1.5
+  // There are three types of edits that can be performed on strings: insert a character, remove a
+  // character, or replace a character. Given two strings, write a function to check if they are one
+  // edit (or zero edits) away.
+  // EXAMPLE
+  // pale,  ple  -> true
+  // pales, pale -> true
+  // pale,  bale -> true
+  // pale,  bake -> false
+
+  s1_length = s1.length;
+  s2_length = s2.length;
+  // If one string is more than a character longer, then one operation cannot make them equivalent.
+  if (Math.abs(s1_length - s2_length) > 1) {
+    return false;
+  } else {
+    let i = 0;
+    // Find the index of the first string difference.
+    while (s1.charAt(i) === s2.charAt(i) && i < s1_length && i < s2_length) {
+      i++;
+    }
+    // If the strings are the same length, and the strings are identical after i,
+    // then a single replace edit will make them equivalent.
+    if (s1_length === s2_length) {
+      return s2.slice(i + 1, s2_length) === s1.slice(i + 1, s1_length);
+    } else {
+      // Remove the character at i from the longer string.
+      // Remove and insert can be caught with one test.
+      s1_length > s2_length
+        ? (s1 = s1.slice(0, i) + s1.slice(i + 1, s1_length))
+        : (s2 = s2.slice(0, i) + s2.slice(i + 1, s2_length));
+      return s1 === s2;
+    }
+  }
+}
+
+console.log(
+  oneAway1('pale', 'ple') === true,
+  oneAway1('pales', 'pale') === true,
+  oneAway1('pale', 'bale') === true,
+  oneAway1('pale', 'bake') === false
+);
+console.log(
+  oneAway2('pale', 'ple') === true,
+  oneAway2('pales', 'pale') === true,
+  oneAway2('pale', 'bale') === true,
+  oneAway2('pale', 'bake') === false
+);
+console.log(
+  oneAway3('pale', 'ple') === true,
+  oneAway3('pales', 'pale') === true,
+  oneAway3('pale', 'bale') === true,
+  oneAway3('pale', 'bake') === false
+);
+console.log(
+  oneAway4('pale', 'ple') === true,
+  oneAway4('pales', 'pale') === true,
+  oneAway4('pale', 'bale') === true,
+  oneAway4('pale', 'bake') === false
+);
+console.log(
+  oneAway5('pale', 'ple') === true,
+  oneAway5('pales', 'pale') === true,
+  oneAway5('pale', 'bale') === true,
+  oneAway5('pale', 'bake') === false
+);
+console.log(
+  oneAway6('pale', 'ple') === true,
+  oneAway6('pales', 'pale') === true,
+  oneAway6('pale', 'bale') === true,
+  oneAway6('pale', 'bake') === false
+);
+console.log(
+  oneAway7('pale', 'ple') === true,
+  oneAway7('pales', 'pale') === true,
+  oneAway7('pale', 'bale') === true,
+  oneAway7('pale', 'bake') === false
+);
+// console.log(
+//   oneAway8('pale', 'ple') === true,
+//   oneAway8('pales', 'pale') === true,
+//   oneAway8('pale', 'bale') === true,
+//   oneAway8('pale', 'bake') === false
+// );
+// console.log(
+//   oneAway9('pale', 'ple') === true,
+//   oneAway9('pales', 'pale') === true,
+//   oneAway9('pale', 'bale') === true,
+//   oneAway9('pale', 'bake') === false
+// );
