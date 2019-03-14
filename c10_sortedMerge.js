d146552dd908832ea0fdfd013ce8d30764867003
milleriishaun <milleriishaun@gmail.com>
2019-03-14 11:54:51 -0700

    Add sortedMerge initial template

---
+++ c10_sortedMerge.js
@@ -0,0 +1,68 @@
+var Benchmark = require('benchmark');
+var suite = new Benchmark.Suite();
+
+// add tests
+suite
+  .add('tripleStep1', function() {
+    tripleStep1(10);
+  })
+  .add('tripleStep2', function() {
+    tripleStep2(10);
+  })
+  .add('tripleStep3', function() {
+    tripleStep3(10);
+  })
+  .add('tripleStep4', function() {
+    tripleStep4(10);
+  })
+  .add('tripleStep5', function() {
+    tripleStep5(10);
+  })
+  .add('tripleStep6', function() {
+    tripleStep6(10);
+  })
+  .add('tripleStep7', function() {
+    tripleStep7(10);
+  })
+  // add listeners
+  .on('cycle', function(event) {
+    console.log(String(event.target));
+  })
+  .on('complete', function() {
+    console.log('Fastest is ' + this.filter('fastest').map('name'));
+  })
+  // run async
+  .run({ async: true });
+
+// CTCI JS (good full c10, with time and space clearly shown)
+// algo sol (ok for c10, missing q:4,6,7,9,10,11, clear format, <10 months old)
+// stackhouse (only q:1,3)
+
+// Just try to have CTCI JS...
+// Just accommodate algo sol and stackhouse.
+// (these are just for reference/roundedness)
+
+// CTCI JS
+// algo sol
+// stackhouse
+
+/** SORTED MERGE
+ * I:
+ * O:
+ * C:
+ * E:
+ */
+
+// Time Complexity: O(n)
+// Time Complexity: O(n)
+// Try 1
+
+console.log(sortedMerge(5));
+console.log(sortedMerge(5));
+console.log(sortedMerge(5));
+console.log(sortedMerge(5));
+console.log(sortedMerge(5));
+console.log(sortedMerge(5));
+console.log(sortedMerge(5));
+
+// results:
