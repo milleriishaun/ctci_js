6052db9c6401802a5c78b37ca570323ae0b099c7
milleriishaun <milleriishaun@gmail.com>
2019-03-17 23:51:49 -0700

    Edit c10_sortedMerge.js

--- c10_sortedMerge.js
+++ c10_sortedMerge.js
@@ -1,38 +1,38 @@
-var Benchmark = require('benchmark');
-var suite = new Benchmark.Suite();
+// var Benchmark = require('benchmark');
+// var suite = new Benchmark.Suite();
 
-// add tests
-suite
-  .add('tripleStep1', function() {
-    tripleStep1(10);
-  })
-  .add('tripleStep2', function() {
-    tripleStep2(10);
-  })
-  .add('tripleStep3', function() {
-    tripleStep3(10);
-  })
-  .add('tripleStep4', function() {
-    tripleStep4(10);
-  })
-  .add('tripleStep5', function() {
-    tripleStep5(10);
-  })
-  .add('tripleStep6', function() {
-    tripleStep6(10);
-  })
-  .add('tripleStep7', function() {
-    tripleStep7(10);
-  })
-  // add listeners
-  .on('cycle', function(event) {
-    console.log(String(event.target));
-  })
-  .on('complete', function() {
-    console.log('Fastest is ' + this.filter('fastest').map('name'));
-  })
-  // run async
-  .run({ async: true });
+// // add tests
+// suite
+//   .add('sortedMerge1', function() {
+//     sortedMerge1([5, 1, 3, 6, 4, 2]);
+//   })
+//   .add('tripleStep2', function() {
+//     tripleStep2(10);
+//   })
+//   .add('tripleStep3', function() {
+//     tripleStep3(10);
+//   })
+//   .add('tripleStep4', function() {
+//     tripleStep4(10);
+//   })
+//   .add('tripleStep5', function() {
+//     tripleStep5(10);
+//   })
+//   .add('tripleStep6', function() {
+//     tripleStep6(10);
+//   })
+//   .add('tripleStep7', function() {
+//     tripleStep7(10);
+//   })
+//   // add listeners
+//   .on('cycle', function(event) {
+//     console.log(String(event.target));
+//   })
+//   .on('complete', function() {
+//     console.log('Fastest is ' + this.filter('fastest').map('name'));
+//   })
+//   // run async
+//   .run({ async: true });
 
 // CTCI JS (good full c10, with time and space clearly shown)
 // algo sol (ok for c10, missing q:4,6,7,9,10,11, clear format, <10 months old)
@@ -55,7 +55,7 @@ suite
 
 // Time Complexity: O(n)
 // Time Complexity: O(n)
-// Try 1
+// Try 1 //
 
 console.log(sortedMerge(5));
 console.log(sortedMerge(5));
